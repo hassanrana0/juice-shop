ad614a36a531810b28f9ace67ed862448748c79a
Muhammad Hassan <hassanrana80808@gmail.com>
2025-11-27 10:04:31 +0500

    Refactor Gruntfile.js for cleaner syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,12 @@
  * SPDX-License-Identifier: MIT
  */
 
-'use strict';
+'use strict'
 
 module.exports = function (grunt) {
-  const os = grunt.option('os') || process.env.PCKG_OS_NAME || '';
-  const platform = grunt.option('platform') || process.env.PCKG_CPU_ARCH || '';
-  const node =
-    grunt.option('node') ||
-    process.env.nodejs_version ||
-    process.env.PCKG_NODE_VERSION ||
-    '';
+  const os = grunt.option('os') || process.env.PCKG_OS_NAME || ''
+  const platform = grunt.option('platform') || process.env.PCKG_CPU_ARCH || ''
+  const node = grunt.option('node') || process.env.nodejs_version || process.env.PCKG_NODE_VERSION || ''
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -20,10 +16,10 @@ module.exports = function (grunt) {
     replace_json: {
       manifest: {
         src: 'package.json',
-        replacements: {
-          'engines.node': node || '<%= pkg.engines.node %>',
-          os: os ? [os] : '<%= pkg.os %>',
-          cpu: platform ? [platform] : '<%= pkg.cpu %>'
+        changes: {
+          'engines.node': (node || '<%= pkg.engines.node %>'),
+          os: (os ? [os] : '<%= pkg.os %>'),
+          cpu: (platform ? [platform] : '<%= pkg.cpu %>')
         }
       }
     },
@@ -32,12 +28,7 @@ module.exports = function (grunt) {
       pckg: {
         options: {
           mode: os === 'linux' ? 'tgz' : 'zip',
-          archive:
-            'dist/<%= pkg.name %>-<%= pkg.version %>' +
-            (node ? '_node' + node : '') +
-            (os ? '_' + os : '') +
-            (platform ? '_' + platform : '') +
-            (os === 'linux' ? '.tgz' : '.zip')
+          archive: 'dist/<%= pkg.name %>-<%= pkg.version %>' + (node ? ('_node' + node) : '') + (os ? ('_' + os) : '') + (platform ? ('_' + platform) : '') + (os === 'linux' ? '.tgz' : '.zip')
         },
         files: [
           {
@@ -71,35 +62,30 @@ module.exports = function (grunt) {
               'uploads/complaints/.gitkeep',
               'views/**'
             ],
-            dest: 'juice-shop_<%= pkg.version %>'
+            dest: 'juice-shop_<%= pkg.version %>/'
           }
         ]
       }
     }
-  });
+  })
 
   grunt.registerTask('checksum', 'Create .md5 checksum files', function () {
-    const fs = require('node:fs');
-    const crypto = require('node:crypto');
+    const fs = require('node:fs')
+    const crypto = require('node:crypto')
+    fs.readdirSync('dist/').forEach(file => {
+      const buffer = fs.readFileSync('dist/' + file)
+      const md5 = crypto.createHash('md5')
+      md5.update(buffer)
+      const md5Hash = md5.digest('hex')
+      const md5FileName = 'dist/' + file + '.md5'
+      grunt.file.write(md5FileName, md5Hash)
+      grunt.log.write(`Checksum ${md5Hash} written to file ${md5FileName}.`).verbose.write('...').ok()
+      grunt.log.writeln()
+    })
+  })
 
-    fs.readdirSync('dist/').forEach((file) => {
-      const buffer = fs.readFileSync('dist/' + file);
-      const md5 = crypto.createHash('md5');
-      md5.update(buffer);
-      const md5Hash = md5.digest('hex');
-      const md5FileName = 'dist/' + file + '.md5';
-
-      grunt.file.write(md5FileName, md5Hash);
-      grunt.log
-        .write(`Checksum ${md5Hash} written to file ${md5FileName}.`)
-        .verbose.write('...')
-        .ok();
-      grunt.log.writeln();
-    });
-  });
-
-  grunt.loadNpmTasks('grunt-replace-json');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-
-  grunt.registerTask('package', ['replace_json:manifest', 'compress:pckg', 'checksum']);
-};
+  grunt.loadNpmTasks('grunt-replace-json')
+  grunt.loadNpmTasks('grunt-contrib-compress')
+  grunt.registerTask('package', ['replace_json:manifest', 'compress:pckg', 'checksum'])
+}
+correct this chunk of code 
